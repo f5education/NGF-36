33e37bba151d02d91d2aabe2315bd50dbc21176c
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-01-31 12:20:11 -0500

    Update script to modify /etc/hosts and add newline

--- 10-NGF-36.sh
+++ 10-NGF-36.sh
@@ -4,9 +4,10 @@ PS4='+$(date +"%T.%3N"): '
 
 # commands for NGF-36 labs
 mkdir ~/.kube
-echo "10.10.1.180        k8s.f5trn.com k8s" | sudo tee --append /etc/hosts
+echo -e "\n10.10.1.180     k8s.f5trn.com k8s" | sudo tee --append /etc/hosts
 ssh-keyscan -H k8s >> ~/.ssh/known_hosts
 scp k8s:~/.kube/config ~/.kube
 sed --in-place 's/127.0.0.1/k8s/' ~/.kube/config
 echo -e "\nKUBECONFIG=~/.kube/config" >> ~/.bashrc
 sudo snap install helm --classic
+kubectl delete node nginx    # for some reason this non-existent "node" is "sticky"
